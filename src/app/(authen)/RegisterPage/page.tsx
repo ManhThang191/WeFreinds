fdcf7d3542b287025c3e9f49eb055f23467ed81e
Manh Thang <thangnmjob.com>
2025-06-27 16:38:41 +0700

    UI register page

---
+++ src/app/(authen)/RegisterPage/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RegisterPage from './RegisterPage';
+
+const page = () => {
+  return (
+    <>
+      <RegisterPage />
+    </>
+  );
+};
+
+export default page;
